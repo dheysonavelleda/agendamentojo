9e849330aadc873cffecb4b9b7504d06f0254c62
Joana <joana@examplo.com>
2026-01-16 20:08:03 +0100

    feat: Adiciona logs no servidor para depurar busca de serviços

--- src/app/(logged)/booking/page.tsx
+++ src/app/(logged)/booking/page.tsx
@@ -2,7 +2,9 @@ import { getServices } from "@/app/actions/get-services";
 import { BookingClient } from "./booking-client";
 
 export default async function BookingPage() {
+  console.log("--- Server Component: Buscando serviços do banco de dados... ---");
   const services = await getServices();
+  console.log("--- Server Component: Serviços encontrados: ---", services);
 
   return <BookingClient initialServices={services || []} />;
 }
